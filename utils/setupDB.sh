2cb9df5f83f29f1f61196916b512b57ebd537aa3
Martin <cph-ms782@cphbusiness.dk>
2020-05-25 09:39:45 +0200

    update

--- utils/setupDB.sh
+++ utils/setupDB.sh
@@ -1,4 +1,4 @@
-#sequelize init
+# sequelize init
 
 sequelize model:create --name User --attributes username:string,email:string,password:string
 sequelize model:create --name ResetToken --attributes email:string,token:string,expiration:date,used:integer
